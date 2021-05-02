76fe0fe8160d28b7e46f66f48ce5a489f80f3ddc
Chinthaka Dharmasiri <chinthakadd@gmail.com>
2021-05-02 18:22:40 -0400

    noted classes to keep an eye

--- Readme.md
+++ Readme.md
@@ -6,5 +6,7 @@ Use the following command to run redis.
 docker run -p 6379:6379 -it --name some-redis -d redis
 ```
 
+## Redis Serialization
+
 Refer to `com.chinthakad.common.session.GenericSessionDataJdkRedisSerializer` on a custom `RedisSerializer` that is
  capable to avoiding deserialization failures on `@SessionScope` beans that may be used in our micro-services.
--- login-app/src/main/java/com/chinthakad/LoginApp.java
+++ login-app/src/main/java/com/chinthakad/LoginApp.java
@@ -3,6 +3,16 @@ package com.chinthakad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * TODO: Write down notes here.
+ *
+ * @see org.springframework.session.data.redis.RedisIndexedSessionRepository
+ * @see org.springframework.session.data.redis.RedisSessionExpirationPolicy
+ * @see org.springframework.session.web.http.SessionEventHttpSessionListenerAdapter
+ * @see org.springframework.security.web.session.HttpSessionEventPublisher
+ * @see org.springframework.session.ReactiveSessionRepository
+ * @see org.springframework.session.data.redis.ReactiveRedisSessionRepository
+ */
 @SpringBootApplication
 public class LoginApp {
 
